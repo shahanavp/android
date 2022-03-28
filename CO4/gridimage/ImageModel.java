042de7ad52e3458e204fcafad6d618b76b1446aa
shahanavp <79835469+shahanavp@users.noreply.github.com>
2022-03-29 01:14:33 +0530

    commit

---
+++ CO4/gridimage/ImageModel.java
@@ -0,0 +1,14 @@
+package com.example.gridimagenew;
+
+public class ImageModel {
+    private String img_name;
+    public ImageModel(int imgid){
+        this.img_name=img_name;
+    }
+    public String getImg_name(){
+        return img_name;
+    }
+    public void setImg_name(String img_name){
+        this.img_name=img_name;
+    }
+}
